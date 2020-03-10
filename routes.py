a54b56e4216c689e93dca906c19c1b59dc24c0b4
nariman9119 <nariman9119@gmail.com>
2020-03-11 00:16:33 +0300

    flask minimal

---
+++ routes.py
@@ -0,0 +1,33 @@
+from app import app
+from flask import request
+import os
+from flask import Flask, render_template
+from model.predict import beauty_predict
+import pandas as pd
+from PIL import Image
+
+    
+@app.route("/")
+def home():
+    
+    return "Welcome!"
+
+@app.route("/predict/<photo>")
+def predict(photo=None):
+    path = 'outputs/' + photo + '.output.jpg'
+    csv_path = 'static/outputs/scores.csv' 
+    scores = pd.read_csv(csv_path)
+    
+        
+    try:
+        im = Image.open('static/'+path)
+        score = scores.loc[scores['name'] == photo, 'score'].iloc[0]
+        
+    except:
+        
+        score = beauty_predict('static/' + photo + '.jpg')
+        row = pd.DataFrame([[photo,score]], columns=['name', 'score'])
+        scores = scores.append(row)
+        scores.to_csv(csv_path)
+
+    return render_template("prediction.html", path = path, score = score)
